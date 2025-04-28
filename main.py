0cbfb58b724cd14661657923ab1b581b7d8bfbf8
Sofi077 <sofia.levchuk@lll.kpi.ua>
2025-04-28 22:10:37 +0300

    Stage3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from connectDB import connect_db
 from readData import read_data
 from createDB import create_database
-from migration import migrate
+from migration import migrate, run_flyway_migrations
+
 
 def main():
 
@@ -12,10 +13,10 @@ def main():
 
 
     # migrate(engine)
-    #
-    #
+
     # read_data(Session)
 
+    run_flyway_migrations()
 
 if __name__ == '__main__':
     main()
--- migration.py
+++ migration.py
@@ -4,6 +4,13 @@ import subprocess
 def migrate(engine):
     Base.metadata.create_all(engine)
 
-
+def run_flyway_migrations():
+    """Запускає Flyway міграції."""
+    try:
+        #subprocess.run([r"C:\Program Files\Red Gate\Flyway Desktop\flyway.cmd", "baseline"], check=True)
+        subprocess.run([r"C:\Program Files\Red Gate\Flyway Desktop\flyway.cmd", "migrate"], check=True)
+        print("Міграції виконано")
+    except subprocess.CalledProcessError as e:
+        print(f"Помилка при виконанні: {e}")
 
 
